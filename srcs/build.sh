c60da4af3e504110627e918f3e9cba2889398e3e
miktim <miktim@mail.ru>
2020-05-21 10:13:37 +0300

    first commit

--- srcs/build.sh
+++ srcs/build.sh
@@ -12,7 +12,7 @@ javac -Xstdout ./compile.log -Xlint:unchecked -cp ${cpath} -d ./ \
 if [ $? -eq 0 ] ; then
   jar cvf ./${jname}.jar ${cpath}/*.class
 #  javadoc -d ./${jname}Doc -nodeprecated -use package-info.java \
-#  WsHandler.java WsConnection.java WsServer.java WssServer.java
+#  JSON.java
 fi
 rm -f -r ./org
 more < ./compile.log
