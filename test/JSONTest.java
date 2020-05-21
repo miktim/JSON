91fd66342f28d92b7cbb377ec0b4149ad0728ea9
miktim <miktim@mail.ru>
2020-05-21 10:21:37 +0300

    FileReader added

--- test/JSONTest.java
+++ test/JSONTest.java
@@ -1,12 +1,18 @@
 /*
  * JSONTest, MIT (c) 2020 miktim@mail.ru
  */
+import java.io.File;
+import java.io.FileReader;
 import java.math.BigDecimal;
 import org.samples.java.JSON;
 
 public class JSONTest {
 
     public static void main(String[] args) throws Exception {
+        String path = (new File(".")).getAbsolutePath();
+        if (args.length > 0) {
+            path = args[0];
+        }
         JSON json = new JSON();
         json.set("Escaped", new String(new char[]{0x8, 0x9, 0xA, 0xC, 0xD, 0x22, 0x2F, 0x5C, 0, '-', 0x1F, 0xd834, 0xdd1e}))
                 .set("EmptyJSON", new JSON())
@@ -22,12 +28,15 @@ public class JSONTest {
         System.out.println(json.get("MinInt").getClass().getSimpleName()
                 + " is Number? "
                 + (json.get("MinInt") instanceof Number));
-//        System.out.println(JSON.parse("\uD834\uDD1E"));
         System.out.println((JSON) JSON.parse(json.stringify()));
         System.out.println(JSON.escapeString((String) json.get("Escaped")));
         System.out.println(json.clone());
         System.out.println(((Number) json.get("Double")).longValue());
 // examples from RFC 8259        
+        System.out.println(JSON.parse("\"\uD834\uDD1E\"")); // G-clef
+        System.out.println(JSON.parse("3.141592653589793238462643383279"));
+        System.out.println(JSON.parse("null"));
+
         String example1 = "{\n"
                 + "        \"Image\": {\n"
                 + "            \"Width\":  800,\n"
@@ -73,6 +82,19 @@ public class JSONTest {
                 + "      ]";
         object = JSON.parse(example2);
         System.out.println(JSON.stringify(((Object[]) object)[1]));
+// Example from https://docs.oracle.com/en/database/oracle/oracle-database/12.2/adjsn/json-data.html#GUID-FBC22D72-AA64-4B0A-92A2-837B32902E2C        
+        object = JSON.parse(new FileReader(new File(path,"json.json")));
+        System.out.println(((JSON)object).get("AllowPartialShipment"));
+// Exceptions        
+//        System.out.println(JSON.parse("\"asfas\\uD83\uDD1E\"")); // unparseable surrogate
+//        System.out.println(JSON.parse("  \"asfas\uD834\\uDD1\"")); // unparseable surrogate
+//        System.out.println(JSON.parse("123e")); // unparseable number
+//        System.out.println(JSON.parse("123 e")); // EOT Expected
+//        System.out.println(JSON.parse("{\"Latitude\":  37.371991\n\"")); // "}" expected
+//        System.out.println(JSON.parse("b123")); // unexpected char
+//        System.out.println(JSON.parse("falsen")); // unknown literal
+//        System.out.println(JSON.parse("{\"\":123}")); // empty propNames allowed
+        
     }
 
 }
