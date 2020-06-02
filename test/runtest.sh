1cecf9e53492d7ba356d487bbcd85fd316dd56fe
miktim <miktim@mail.ru>
2020-06-02 07:40:46 +0300

    java 1.7

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo $(javac -version)
+echo $(java -version)
 if [ -f ../srcs/JSON.jar ]; then
-  java -cp ../srcs/JSON.jar JSONTest.java
+#  java -cp ../srcs/JSON.jar JSONTest.java
+  javac -cp ../srcs/JSON.jar JSONTest.java
+  java -cp ../srcs/JSON.jar:. JSONTest
+  rm -f *.class 
 else
   echo First make the ../srcs/JSON.jar file.
 fi
