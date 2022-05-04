588ac624dfe4ee50c6df48e7a11e775b652852e4
miktim <miktim@mail.ru>
2022-05-04 16:18:15 +0300

    2.0,1

--- JSONTestSuite/parsers/test_java_miktim_json_2_0/make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-javac -cp ".:json-2.0.0.jar" TestJSONParsing.java
-jar cvfm TestJSONParsing.jar META-INF/MANIFEST.MF json-2.0.0.jar TestJSONParsing.class
-java -jar ./TestJSONParsing.jar
